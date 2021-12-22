d5c57baa7df5d9dddd5031d5eb90ad656cee3cc1
1nterface <1nterfacedemexico@gmail.com>
2021-12-22 00:50:16 -0800

    Initial commit

---
+++ lib/Clientes/clientes_login.dart
@@ -0,0 +1,78 @@
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:toast/toast.dart';
+import 'package:cryptoplaymates/Modelo/cajas_modelo.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:location_permissions/location_permissions.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cryptoplaymates/authentication.dart';
+
+
+class clientes_login extends StatefulWidget {
+
+  var data;
+  clientes_login({this.data});
+  @override
+  clientes_loginState createState() => clientes_loginState();
+}
+
+class clientes_loginState extends State<clientes_login> {
+
+  clientes_loginState();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: SpeedDial( //Boton flotante animado,
+        //marginRight: 18,
+        //marginBottom: 30,
+        animatedIcon: AnimatedIcons.home_menu,
+        animatedIconTheme: IconThemeData(size: 25.0),
+        // this is ignored if animatedIcon is non null
+        // child: Icon(Icons.add),
+        visible: true,
+        curve: Curves.bounceIn,
+        overlayColor: Colors.black,
+        overlayOpacity: 0.5,
+        onOpen: () => print('OPENING DIAL'),
+        onClose: () => print('DIAL CLOSED'),
+        tooltip: 'Speed Dial',
+        heroTag: 'speed-dial-hero-tag',
+        backgroundColor: Colors.purple[800],
+        foregroundColor: Colors.white,
+        elevation: 1.0,
+        shape: CircleBorder(),
+        children: [
+
+          SpeedDialChild(
+              child: Icon(Icons.home, color: Colors.white,),
+              backgroundColor: Colors.purple[800],
+              label: 'Informacion',
+              onTap: () async {
+
+                //Navigator.of(context).pushNamed('/admin_inicio');
+
+              }
+          ),
+
+        ],
+      ),
+      backgroundColor: Colors.black,
+      body: Image.asset("image/pizza.png"),
+    );
+  }
+}
