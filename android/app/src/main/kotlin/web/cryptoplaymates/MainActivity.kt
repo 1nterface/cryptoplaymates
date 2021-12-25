a4b288f8359e4724bb424b986b075078af7bbd5c
1nterface <1nterfacedemexico@gmail.com>
2021-12-24 16:46:28 -0800

    ur message here

---
+++ android/app/src/main/kotlin/web/cryptoplaymates/MainActivity.kt
@@ -0,0 +1,6 @@
+package web.cryptoplaymates
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+//@dart=2.9
+import 'package:cryptoplaymates/Modelo/agentes_modelo.dart';
+import 'package:cryptoplaymates/Clientes/clientes_login.dart';
+import 'package:cryptoplaymates/olvidecontra.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:location_permissions/location_permissions.dart';
+import 'package:url_strategy/url_strategy.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  setPathUrlStrategy();
+  // Initialize a new Firebase App instance
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+
+  const MyApp({Key key}) : super(key: key);
+  
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.black,
+      debugShowCheckedModeBanner: false,
+      routes: <String, WidgetBuilder> {
+
+        '/clientes_login': (BuildContext context) => clientes_login(),
+
+      },
+      title: 'Crypto Playmate',
+      theme: ThemeData(
+
+      ),
+      home:
+      //Inicio(),
+      clientes_login(),
+      // <--- App Clientes
+
+
+    );
+  }
+}
