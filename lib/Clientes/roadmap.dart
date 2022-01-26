b6488082821a2df0de70c3a543087af7b4c4fb98
1nterface <1nterfacedemexico@gmail.com>
2022-01-26 01:48:28 -0800

    1.0.73

--- lib/Clientes/roadmap.dart
+++ lib/Clientes/roadmap.dart
@@ -28,52 +28,52 @@ class _roadmapState extends State<roadmap> {
               Stepper(
                 steps: [
                   Step(
-                    title: Text('OPEN 2022 NFT MEMBERSHIP', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    title: Text('OPEN 2022 NFT MEMBERSHIP', style: TextStyle(color: Colors.lightBlueAccent, fontSize: 40),softWrap: true),
                     content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
                   ),
 
                   Step(
-                    title: Text('OPEN 2022 NFT POWER CARDS PRE-SALE', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    title: Text('OPEN 2022 NFT POWER CARDS PRE-SALE', style: TextStyle(color: Colors.lightBlueAccent, fontSize: 40),softWrap: true),
                     content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
                   ),
 
                   Step(
-                    title: Text('NFT MEMBERS & POWER CARDS DEMO', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    title: Text('NFT MEMBERS & POWER CARDS DEMO', style: TextStyle(color: Colors.lightBlueAccent, fontSize: 40),softWrap: true),
                     content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
                   ),
 
                   Step(
-                    title: Text('CRYPTO PLAYMATE OFFICIAL LAUNCH after SUPER BOWL LVI', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    title: Text('CRYPTO PLAYMATE OFFICIAL LAUNCH after SUPER BOWL LVI', style: TextStyle(color: Colors.lightBlueAccent, fontSize: 40),softWrap: true),
                     content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
                   ),
 
                   Step(
-                    title: Text('RECRUIT COMMUNITY MANAGER FOR DISCORD GROUP', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    title: Text('RECRUIT COMMUNITY MANAGER FOR DISCORD GROUP', style: TextStyle(color: Colors.lightBlueAccent, fontSize: 40),softWrap: true),
                     content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
                   ),
 
                   Step(
-                    title: Text('RECRUIT DESIGN TEAM', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    title: Text('RECRUIT DESIGN TEAM', style: TextStyle(color: Colors.lightBlueAccent, fontSize: 40),softWrap: true),
                     content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
                   ),
 
                   Step(
-                    title: Text('RECRUIT FLUTTER DEVS', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    title: Text('RECRUIT FLUTTER DEVS', style: TextStyle(color: Colors.lightBlueAccent, fontSize: 40),softWrap: true),
                     content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
                   ),
 
                   Step(
-                    title: Text('RECRUIT MARKETING TEAM', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    title: Text('RECRUIT MARKETING TEAM', style: TextStyle(color: Colors.lightBlueAccent, fontSize: 40),softWrap: true),
                     content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
                   ),
 
                   Step(
-                    title: Text('NEW GAMES RELESEASE', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    title: Text('NEW GAMES RELESEASE', style: TextStyle(color: Colors.lightBlueAccent, fontSize: 40),softWrap: true),
                     content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
                   ),
 
                   Step(
-                    title: Text('CREATE CRYPTO PLAYMATE FOUNDATION', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    title: Text('CREATE CRYPTO PLAYMATE FOUNDATION', style: TextStyle(color: Colors.lightBlueAccent, fontSize: 40),softWrap: true),
                     content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
                   ),
                 ],
@@ -84,7 +84,7 @@ class _roadmapState extends State<roadmap> {
                 },
                 currentStep: _currentStep,
                 onStepContinue: (){
-                  if (_currentStep != 2){
+                  if (_currentStep != 9){
                     setState(() {
                       _currentStep += 1;
                     });
