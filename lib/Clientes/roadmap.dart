206b80d893a8bec2cd72834bf590c459092dece3
1nterface <1nterfacedemexico@gmail.com>
2022-01-26 01:35:28 -0800

    1.0.73

--- lib/Clientes/roadmap.dart
+++ lib/Clientes/roadmap.dart
@@ -21,89 +21,82 @@ class _roadmapState extends State<roadmap> {
         title: const Text('Roadmap'),
       ),
       body: SingleChildScrollView(
-        child: Container(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Column(
-                  children:  [
-                    Stepper(
-                      steps: [
-                        Step(
-                          title: Text('OPEN 2022 NFT MEMBERSHIP', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                        ),
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            children:  [
+              Stepper(
+                steps: [
+                  Step(
+                    title: Text('OPEN 2022 NFT MEMBERSHIP', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                  ),
 
-                        Step(
-                          title: Text('OPEN 2022 NFT POWER CARDS PRE-SALE', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                        ),
+                  Step(
+                    title: Text('OPEN 2022 NFT POWER CARDS PRE-SALE', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                  ),
 
-                        Step(
-                          title: Text('NFT MEMBERS & POWER CARDS DEMO', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                        ),
+                  Step(
+                    title: Text('NFT MEMBERS & POWER CARDS DEMO', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                  ),
 
-                        Step(
-                          title: Text('CRYPTO PLAYMATE OFFICIAL LAUNCH after SUPER BOWL LVI', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                        ),
+                  Step(
+                    title: Text('CRYPTO PLAYMATE OFFICIAL LAUNCH after SUPER BOWL LVI', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                  ),
 
-                        Step(
-                          title: Text('RECRUIT COMMUNITY MANAGER FOR DISCORD GROUP', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                        ),
+                  Step(
+                    title: Text('RECRUIT COMMUNITY MANAGER FOR DISCORD GROUP', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                  ),
 
-                        Step(
-                          title: Text('RECRUIT DESIGN TEAM', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                        ),
+                  Step(
+                    title: Text('RECRUIT DESIGN TEAM', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                  ),
 
-                        Step(
-                          title: Text('RECRUIT FLUTTER DEVS', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                        ),
+                  Step(
+                    title: Text('RECRUIT FLUTTER DEVS', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                  ),
 
-                        Step(
-                          title: Text('RECRUIT MARKETING TEAM', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                        ),
+                  Step(
+                    title: Text('RECRUIT MARKETING TEAM', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                  ),
 
-                        Step(
-                          title: Text('NEW GAMES RELESEASE', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                        ),
+                  Step(
+                    title: Text('NEW GAMES RELESEASE', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                  ),
 
-                        Step(
-                          title: Text('CREATE CRYPTO PLAYMATE FOUNDATION', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                        ),
-                      ],
-                      onStepTapped: (int newIndex){
-                        setState(() {
-                          _currentStep = newIndex;
-                        });
-                      },
-                      currentStep: _currentStep,
-                      onStepContinue: (){
-                        if (_currentStep != 2){
-                          setState(() {
-                            _currentStep += 1;
-                          });
-                        }
-                      },
-                      onStepCancel: (){
-                        if(_currentStep != 0){
-                          setState(() {
-                            _currentStep -= 1;
-                          });
-                        }
-                      },
-                    ),
-                  ],
-                ),
+                  Step(
+                    title: Text('CREATE CRYPTO PLAYMATE FOUNDATION', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                    content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                  ),
+                ],
+                onStepTapped: (int newIndex){
+                  setState(() {
+                    _currentStep = newIndex;
+                  });
+                },
+                currentStep: _currentStep,
+                onStepContinue: (){
+                  if (_currentStep != 2){
+                    setState(() {
+                      _currentStep += 1;
+                    });
+                  }
+                },
+                onStepCancel: (){
+                  if(_currentStep != 0){
+                    setState(() {
+                      _currentStep -= 1;
+                    });
+                  }
+                },
               ),
             ],
           ),
