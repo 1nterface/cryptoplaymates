7c26bbd4b44f31e1ddff14f37ce8aec36fb30e57
1nterface <1nterfacedemexico@gmail.com>
2022-01-26 01:24:25 -0800

    1.0.71

--- lib/Clientes/roadmap.dart
+++ lib/Clientes/roadmap.dart
@@ -8,6 +8,9 @@ class roadmap extends StatefulWidget {
 }
 
 class _roadmapState extends State<roadmap> {
+
+  int _currentStep = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,74 +29,59 @@ class _roadmapState extends State<roadmap> {
                 padding: const EdgeInsets.all(25.0),
                 child: Column(
                   children:  [
-                    const Text('I', style: const TextStyle(color: Colors.white, fontSize: 40),),
-                    const SizedBox(width: 20,),
-                    Text('OPEN 2022 NFT MEMBERSHIP', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('II', style: TextStyle(color: Colors.white, fontSize: 40),),
-                    const SizedBox(width: 20,),
-                    Text('OPEN 2022 NFT POWER CARDS PRE-SALE', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('Short description', style: const TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                    const SizedBox(width: 40,),
+                    Stepper(
+                      steps: [
+                        Step(
+                          title: Text('OPEN 2022 NFT MEMBERSHIP', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                        ),
 
-                    const Text('III', style: const TextStyle(color: Colors.white, fontSize: 40),),
-                    const SizedBox(width: 20,),
-                    Text('NFT MEMBERS & POWER CARDS DEMO', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                    const SizedBox(width: 40,),
+                        Step(
+                          title: Text('OPEN 2022 NFT POWER CARDS PRE-SALE', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                        ),
 
-                    const Text('IV', style: TextStyle(color: Colors.white, fontSize: 40, ),),
-                    const SizedBox(width: 20,),
-                    Text('CRYPTO PLAYMATE OFFICIAL LAUNCH after SUPER BOWL LVI', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                    const SizedBox(width: 40,),
+                        Step(
+                          title: Text('NFT MEMBERS & POWER CARDS DEMO', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                        ),
 
-                    const Text('V', style: TextStyle(color: Colors.white, fontSize: 40),),
-                    const SizedBox(width: 20,),
-                    Text('RECRUIT COMMUNITY MANAGER FOR DISCORD GROUP', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                    const SizedBox(width: 40,),
+                        Step(
+                          title: Text('CRYPTO PLAYMATE OFFICIAL LAUNCH after SUPER BOWL LVI', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                        ),
 
-                    const Text('VI', style: TextStyle(color: Colors.white, fontSize: 40),),
-                    const SizedBox(width: 20,),
-                    Text('RECRUIT DESIGN TEAM', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                    const SizedBox(width: 40,),
+                        Step(
+                          title: Text('RECRUIT COMMUNITY MANAGER FOR DISCORD GROUP', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                        ),
 
-                    const Text('VII', style: TextStyle(color: Colors.white, fontSize: 40),),
-                    const SizedBox(width: 20,),
-                    Text('RECRUIT FLUTTER DEVS', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                    const SizedBox(width: 40,),
+                        Step(
+                          title: Text('RECRUIT DESIGN TEAM', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                        ),
 
-                    const Text('VIII', style: TextStyle(color: Colors.white, fontSize: 40),),
-                    const SizedBox(width: 20,),
-                    Text('RECRUIT MARKETING TEAM', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('Short description', style: const TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                    const SizedBox(width: 40,),
+                        Step(
+                          title: Text('RECRUIT FLUTTER DEVS', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                        ),
 
-                    const Text('IX', style: const TextStyle(color: Colors.white, fontSize: 40),),
-                    const SizedBox(width: 20,),
-                    Text('NEW GAMES RELESEASES', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                    const SizedBox(width: 40,),
+                        Step(
+                          title: Text('RECRUIT MARKETING TEAM', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                        ),
 
-                    const Text('X', style: TextStyle(color: Colors.white, fontSize: 40),),
-                    const SizedBox(width: 20,),
-                    Text('CREATE CRYPTO PLAYMATE FOUNDATION', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
-                    const SizedBox(width: 20,),
-                    const Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
-                    const SizedBox(width: 40,),
+                        Step(
+                          title: Text('NEW GAMES RELESEASE', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                        ),
+
+                        Step(
+                          title: Text('CREATE CRYPTO PLAYMATE FOUNDATION', style: TextStyle(color: Colors.purple[900], fontSize: 40),softWrap: true),
+                          content: Text('Short description', style: TextStyle(color: Colors.white, fontSize: 25),softWrap: true),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
