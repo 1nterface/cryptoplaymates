0f81a60c4519c4174308fd1b84540d7b03efdfdb
1nterface <1nterfacedemexico@gmail.com>
2022-01-26 01:30:17 -0800

    1.0.72

--- lib/Clientes/whitepaper.dart
+++ lib/Clientes/whitepaper.dart
@@ -18,37 +18,39 @@ class whitepaperState extends State<whitepaper> {
         centerTitle: true,
         title: const Text('Whitepaper'),
       ),
-      body: Container(
-        child: SingleChildScrollView(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Column(
-                  children: const [
-                    Text('Executive Summary', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),
-                    SizedBox(width: 20,),
-                    Text('Our Mission', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),
-                    SizedBox(width: 20,),
-                    Text('Our Vision', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
-                    SizedBox(width: 20,),
-                    Text('Hyper-casual games presentation', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
-                    SizedBox(width: 20,),
-                    Text('Instructions', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
-                    SizedBox(width: 20,),
-                    Text('NFT Members', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
-                    SizedBox(width: 20,),
-                    Text('NFT Power Cards', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
-                    SizedBox(width: 20,),
-                    Text('Crypto Playmate Foundation', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
-                    SizedBox(width: 20,),
-                    Text('Team', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
+      body: FittedBox(
+        child: Container(
+          child: SingleChildScrollView(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    children: const [
+                      Text('Executive Summary', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),
+                      SizedBox(width: 20,),
+                      Text('Our Mission', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),
+                      SizedBox(width: 20,),
+                      Text('Our Vision', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
+                      SizedBox(width: 20,),
+                      Text('Hyper-casual games presentation', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
+                      SizedBox(width: 20,),
+                      Text('Instructions', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
+                      SizedBox(width: 20,),
+                      Text('NFT Members', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
+                      SizedBox(width: 20,),
+                      Text('NFT Power Cards', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
+                      SizedBox(width: 20,),
+                      Text('Crypto Playmate Foundation', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
+                      SizedBox(width: 20,),
+                      Text('Team', style: TextStyle(color: Colors.white, fontSize: 40),softWrap: true),SizedBox(width: 20,),
 
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
